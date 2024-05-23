850f1c6c527734db18e0de6764d6ffb975cc260d
vedmitra <g.vedamitra@gmail.com>
2024-05-23 19:33:35 +1000

    Enabled cors for required hosts
    For now, only local

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOrigin = process.env.ALLOWED_ORIGIN;
+
+  app.enableCors({
+    origin: corsOrigin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
