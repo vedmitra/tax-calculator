4c0d357e3c5ab0f7be9bd788e79c99d7c438ab02
vedmitra <g.vedamitra@gmail.com>
2024-05-23 16:25:10 +1000

    Tax service base version
    A very rough one. All calculations inline.
    Not scaleable in the current format.
    DB migration pending.

--- api/src/tax/tax.service.spec.ts
+++ api/src/tax/tax.service.spec.ts
@@ -15,4 +15,109 @@ describe('TaxService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+  it('should calculate tax for income <= $18,200', () => {
+    const { tax, superAnnuation } = service.calculateTax({
+      income: 18200,
+      includeSuper: false,
+      year: '2022-23',
+    });
+    expect(tax).toBeCloseTo(0, 2);
+    expect(superAnnuation).toBeCloseTo(1911, 2);
+
+    // Income including super
+    const { tax: tax2, superAnnuation: superAnnuation2 } = service.calculateTax(
+      {
+        income: 18200,
+        includeSuper: true,
+        year: '2022-23',
+      },
+    );
+    expect(tax2).toBeCloseTo(0);
+    expect(superAnnuation2).toBeCloseTo(1729.41, 2);
+  });
+
+  it('should calculate tax for income between $18,201 and $45,000', () => {
+    const { tax, superAnnuation } = service.calculateTax({
+      income: 30000,
+      includeSuper: false,
+      year: '2022-23',
+    });
+    expect(tax).toBeCloseTo(2242, 2);
+    expect(superAnnuation).toBeCloseTo(3150, 2);
+
+    // Income including super
+    const { tax: tax2, superAnnuation: superAnnuation2 } = service.calculateTax(
+      {
+        income: 30000,
+        includeSuper: true,
+        year: '2022-23',
+      },
+    );
+    expect(tax2).toBeCloseTo(1700.37, 2);
+    expect(superAnnuation2).toBeCloseTo(2850.68, 2);
+  });
+
+  it('should calculate tax for income between $45,001 and $120,000', () => {
+    const { tax, superAnnuation } = service.calculateTax({
+      income: 60000,
+      includeSuper: false,
+      year: '2022-23',
+    });
+    expect(tax).toBeCloseTo(9967, 2);
+    expect(superAnnuation).toBeCloseTo(6300, 2);
+
+    // Income including super
+    const { tax: tax2, superAnnuation: superAnnuation2 } = service.calculateTax(
+      {
+        income: 60000,
+        includeSuper: true,
+        year: '2022-23',
+      },
+    );
+    expect(tax2).toBeCloseTo(8114.06, 2);
+    expect(superAnnuation2).toBeCloseTo(5701.36, 2);
+  });
+
+  it('should calculate tax for income between $120,001 and $180,000', () => {
+    const { tax, superAnnuation } = service.calculateTax({
+      income: 150000,
+      includeSuper: false,
+      year: '2022-23',
+    });
+    expect(tax).toBeCloseTo(40567, 2);
+    expect(superAnnuation).toBeCloseTo(15750, 2);
+
+    // Income including super
+    const { tax: tax2, superAnnuation: superAnnuation2 } = service.calculateTax(
+      {
+        income: 150000,
+        includeSuper: true,
+        year: '2022-23',
+      },
+    );
+    expect(tax2).toBeCloseTo(35293.24, 2);
+    expect(superAnnuation2).toBeCloseTo(14253.39, 2);
+  });
+
+  it('should calculate tax for income over $180,001', () => {
+    const { tax, superAnnuation } = service.calculateTax({
+      income: 200000,
+      includeSuper: false,
+      year: '2022-23',
+    });
+    expect(tax).toBeCloseTo(63232, 2);
+    expect(superAnnuation).toBeCloseTo(21000, 2);
+
+    // Income including super
+    const { tax: tax2, superAnnuation: superAnnuation2 } = service.calculateTax(
+      {
+        income: 200000,
+        includeSuper: true,
+        year: '2022-23',
+      },
+    );
+    expect(tax2).toBeCloseTo(54679.96, 2);
+    expect(superAnnuation2).toBeCloseTo(19004.52, 2);
+  });
+  // TODO: Test for other years' tax rates when known
 });
--- api/src/tax/tax.service.ts
+++ api/src/tax/tax.service.ts
@@ -1,4 +1,99 @@
 import { Injectable } from '@nestjs/common';
 
+type TaxInput = {
+  income: number;
+  year: string;
+  includeSuper: boolean;
+  superannuationRate?: number;
+};
+
+type TaxRate = {
+  baseLimit: number;
+  upperLimit: number;
+  baseTax: number;
+  rate: number;
+};
+
+type SuperRate = {
+  year: string;
+  rate: number;
+};
+
 @Injectable()
-export class TaxService {}
+export class TaxService {
+  // TODO: Error handling and logging.
+  calculateTax(input: TaxInput): { tax: number; superAnnuation: number } {
+    const { income, year, includeSuper, superannuationRate } = input;
+    const superRate = superannuationRate || this.getSuperRate(year);
+    if (superRate === null) {
+      throw new Error('Invalid year');
+    }
+    const superAnnuation = this.calculateSuperanuation(
+      income,
+      includeSuper,
+      superRate,
+    );
+    let taxableIncome = income;
+
+    if (includeSuper && superRate !== null) {
+      taxableIncome = income - superAnnuation;
+    }
+
+    const taxRates = this.getTaxRates();
+
+    let tax = 0;
+
+    for (const bracket of taxRates) {
+      if (
+        taxableIncome >= bracket.baseLimit &&
+        taxableIncome <= bracket.upperLimit
+      ) {
+        tax =
+          (taxableIncome - bracket.baseLimit + 1) * bracket.rate +
+          bracket.baseTax;
+        break;
+      }
+    }
+
+    return { tax, superAnnuation };
+  }
+
+  private getTaxRates(): TaxRate[] {
+    // In the future, fetch tax rates from the database
+    const taxRates: TaxRate[] = [
+      { baseLimit: 0, upperLimit: 18200, baseTax: 0, rate: 0 },
+      { baseLimit: 18201, upperLimit: 45000, baseTax: 0, rate: 0.19 },
+      { baseLimit: 45001, upperLimit: 120000, baseTax: 5092, rate: 0.325 },
+      { baseLimit: 120001, upperLimit: 180000, baseTax: 29467, rate: 0.37 },
+      { baseLimit: 180001, upperLimit: Infinity, baseTax: 54232, rate: 0.45 },
+    ];
+
+    return taxRates;
+  }
+
+  private getSuperRates(): SuperRate[] {
+    // In the future, fetch super rates from the database
+    const superRates: SuperRate[] = [
+      { year: '2022-23', rate: 10.5 },
+      { year: '2023-24', rate: 11 },
+    ];
+    return superRates;
+  }
+
+  private getSuperRate(year: string): number | null {
+    const superRates = this.getSuperRates();
+    const superRateObj = superRates.find((rate) => rate.year === year);
+    return superRateObj ? superRateObj.rate : null;
+  }
+
+  private calculateSuperanuation(
+    income: number,
+    includeSuper = false,
+    rate: number,
+  ) {
+    const superannuation = includeSuper
+      ? (income * rate) / 100 / (1 + rate / 100)
+      : (income * rate) / 100;
+    return superannuation;
+  }
+}
